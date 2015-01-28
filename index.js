d0d8c05b2b300fbb19fe73fe46d65b0ac2f8c984
Stephan Bönnemann <stephan@boennemann.me>
2015-01-28 13:04:40 +0100

    feat: initial bot

---
+++ index.js
@@ -0,0 +1,35 @@
+var slackChannel = process.env.SLACK_CHANNEL
+var ircChannel = process.env.IRC_CHANNEL
+
+var http = require('http')
+
+var Slack = require('slack-client')
+var slack = new Slack(process.env.SLACK_TOKEN , true, true)
+
+var irc = require('irc')
+var client = new irc.Client('irc.freenode.net', 'slackbot', {
+    channels: [ircChannel],
+})
+
+slack.on('message', function(message) {
+  var channel = slack.getChannelGroupOrDMByID(message.channel).name
+  var user = slack.getUserByID(message.user).name
+  var text = message.text
+
+  if (channel !== slackChannel) return
+  client.say(ircChannel, user + ': ' + text)
+})
+
+slack.login()
+
+client.addListener('message', function (user, channel, text) {
+  if (channel !== ircChannel) return
+  slack.getChannelByName(slackChannel).send(user + ': ' + text)
+})
+
+var server = http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'})
+  res.end('beep boop\nSyncing Slack#' + slackChannel + ' to IRC' + ircChannel)
+})
+
+server.listen(process.env.PORT || 8000);
